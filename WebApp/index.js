54d955a47cf0b1e177f6c450fff2ca540ee38b10
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-08-18 17:22:56 +0530

    updated

--- WebApp/index.js
+++ WebApp/index.js
@@ -7,9 +7,10 @@ const port=3000;
 
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+
 let todos = [];
  var currentDate=new Date();
 
